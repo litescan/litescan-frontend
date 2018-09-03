45c2211322f72cabe7ab6d3ed71bc6579e3f5293
Roy van Kaathoven <ik@royvankaathoven.nl>
2018-09-03 14:53:20 +0200

    cleanup

--- src/components/blockchain/ChainParameters.js
+++ src/components/blockchain/ChainParameters.js
@@ -2,11 +2,10 @@ import React, {Component} from 'react';
 import {trim} from "lodash";
 import {tu} from "../../utils/i18n";
 import {connect} from "react-redux";
-import {BlockNumberLink} from "../common/Links";
-import {FormattedNumber} from "react-intl";
 import {Client} from "../../services/api";
 import xhr from "axios";
 import {TRXPrice} from "../common/Price";
+import {ONE_TRX} from "../../constants";
 
 
 function ParameterRow({ label, body }) {
@@ -27,18 +26,18 @@ class ChainParameters extends Component {
       parameters: {},
       parameterRenderers: {
         MAINTENANCE_TIME_INTERVAL: time => <span>{time / 1000 / 60} {tu("minutes")}</span>,
-        ACCOUNT_UPGRADE_COST: value => <span><TRXPrice amount={value}/></span>,
-        CREATE_ACCOUNT_FEE: value => <span><TRXPrice amount={value}/></span>,
-        TRANSACTION_FEE: value => <span><TRXPrice amount={value}/></span>,
-        ASSET_ISSUE_FEE: value => <span><TRXPrice amount={value}/></span>,
-        WITNESS_PAY_PER_BLOCK: value => <span><TRXPrice amount={value}/></span>,
-        WITNESS_STANDBY_ALLOWANCE: value => <span><TRXPrice amount={value}/></span>,
-        CREATE_NEW_ACCOUNT_FEE_IN_SYSTEM_CONTRACT: value => <span><TRXPrice amount={value}/></span>,
-        CREATE_NEW_ACCOUNT_BANDWIDTH_RATE: value => <span>{value}</span>,
-        ALLOW_CREATION_OF_CONTRACTS: value => <span>{ value ? tu("yes") : tu("no") }</span>,
-        REMOVE_THE_POWER_OF_THE_GR: value => <span>{ value ? tu("yes") : tu("no") }</span>,
-        ENERGY_FEE: value => <span><TRXPrice amount={value}/></span>,
-        EXCHANGE_CREATE_FEE: value => <span><TRXPrice amount={value}/></span>,
+        ACCOUNT_UPGRADE_COST: value => <TRXPrice amount={value / ONE_TRX}/>,
+        CREATE_ACCOUNT_FEE: value => <TRXPrice amount={value / ONE_TRX}/>,
+        TRANSACTION_FEE: value => <TRXPrice amount={value / ONE_TRX}/>,
+        ASSET_ISSUE_FEE: value => <TRXPrice amount={value / ONE_TRX}/>,
+        WITNESS_PAY_PER_BLOCK: value => <TRXPrice amount={value / ONE_TRX}/>,
+        WITNESS_STANDBY_ALLOWANCE: value => <TRXPrice amount={value / ONE_TRX}/>,
+        CREATE_NEW_ACCOUNT_FEE_IN_SYSTEM_CONTRACT: value => <TRXPrice amount={value / ONE_TRX}/>,
+        CREATE_NEW_ACCOUNT_BANDWIDTH_RATE: value => <span>{value / ONE_TRX}</span>,
+        ALLOW_CREATION_OF_CONTRACTS: value => <span>{value ? tu("yes") : tu("no")}</span>,
+        REMOVE_THE_POWER_OF_THE_GR: value => <span>{value ? tu("yes") : tu("no")}</span>,
+        ENERGY_FEE: value => <TRXPrice amount={value / ONE_TRX}/>,
+        EXCHANGE_CREATE_FEE: value => <TRXPrice amount={value / ONE_TRX}/>,
         MAX_CPU_TIME_OF_ONE_TX: value => <span>{value}</span>,
       }
     };
